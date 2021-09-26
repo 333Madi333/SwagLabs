22f2795a2c9a93991c784ab3b276fc6d1bd813f3
333Madi333 <333madi333@gmail.com>
2021-09-26 13:51:08 -0400

    modified 1.3

--- src/main/java/steps/HomeSteps.java
+++ src/main/java/steps/HomeSteps.java
@@ -5,10 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import utils.SeleniumUtils;
-import utils.WebDriverUtils;
 
-import java.awt.*;
-import java.util.concurrent.TimeUnit;
 
 public class HomeSteps {
     HomeImpl impl = new HomeImpl();
@@ -22,25 +19,6 @@ public class HomeSteps {
     public void i_click_add_to_cart_button_toggle_should_change_remove() {
         impl.getPage().addBtn.click();
         Assert.assertTrue(impl.getPage().removeBtn.isDisplayed());
-    }
-
-
-    @Then("Title of the page should be Twitter")
-    public void title_of_the_page_should_be_twitter() throws InterruptedException {
-//          Assert.assertEquals("Sauce Labs (@saucelabs) / Twitter", SeleniumUtils.switchToNextWindowGetTitle());
-        Assert.assertTrue(SeleniumUtils.switchToNextWindowGetTitle().contains("Twitter"));
-    }
-
-    @Then("Title of the page should be Sauce Labs | Facebook")
-    public void title_of_the_page_should_be_sauce_labs_facebook() {
-//        Assert.assertEquals("Sauce Labs - Home | Facebook", SeleniumUtils.switchToNextWindowGetTitle());
-        Assert.assertTrue(SeleniumUtils.switchToNextWindowGetTitle().contains("Facebook"));
-    }
-
-    @Then("Title of the page should be Sign In | LinkedIn")
-    public void title_of_the_page_should_be_sign_in_linked_in() {
-//        Assert.assertEquals("Sign In | LinkedIn", SeleniumUtils.switchToNextWindowGetTitle());
-        Assert.assertTrue(SeleniumUtils.switchToNextWindowGetTitle().contains("LinkedIn"));
     }
 
     @When("I click {string} button")
